e3b54c911dd245b5417f671ad37912bf44ff169c
Mateus de Castro <ttukstro@gmail.com>
2022-06-21 11:38:21 -0300

    alteracao no nome da variavel da questao 8

--- Lista2/src/exercicio8.java
+++ Lista2/src/exercicio8.java
@@ -6,34 +6,34 @@ public class exercicio8 {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        int[][] vetores = new int[5][4];
+        int[][] matrizes = new int[5][4];
 
         for(int i = 0; i < 5; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.println("Digite o valor da linha " + (i + 1) + " e coluna" + (j + 1));
-                vetores[i][j] = scanner.nextInt();
+                matrizes[i][j] = scanner.nextInt();
             }
         }
 
-       double alunoUm = (vetores[0][1] * 0.6) + (vetores[0][2] * 0.4);
-       double alunoDois = (vetores[1][1] * 0.6) + (vetores[1][2] * 0.4);
-       double alunoTres = (vetores[2][1] * 0.6) + (vetores[2][2] * 0.4);
-       double alunoQuatro = (vetores[3][1] * 0.6) + (vetores[3][2] * 0.4);
-       double alunoCinco = (vetores[4][1] * 0.6) + (vetores[4][2] * 0.4);
+       double alunoUm = (matrizes[0][1] * 0.6) + (matrizes[0][2] * 0.4);
+       double alunoDois = (matrizes[1][1] * 0.6) + (matrizes[1][2] * 0.4);
+       double alunoTres = (matrizes[2][1] * 0.6) + (matrizes[2][2] * 0.4);
+       double alunoQuatro = (matrizes[3][1] * 0.6) + (matrizes[3][2] * 0.4);
+       double alunoCinco = (matrizes[4][1] * 0.6) + (matrizes[4][2] * 0.4);
 
        if(alunoUm > alunoDois && alunoUm > alunoTres && alunoUm > alunoQuatro && alunoUm > alunoCinco){
-           System.out.println("A matricula com a maior nota foi a :" + vetores[0][0]);
+           System.out.println("A matricula com a maior nota foi a :" + matrizes[0][0]);
        } else if (alunoDois > alunoUm && alunoDois > alunoTres && alunoDois > alunoQuatro && alunoDois > alunoCinco) {
-           System.out.println("A matricula com a maior nota foi a :" + vetores[1][0]);
+           System.out.println("A matricula com a maior nota foi a :" + matrizes[1][0]);
        } else if (alunoTres > alunoUm && alunoTres > alunoDois && alunoTres > alunoQuatro && alunoTres > alunoCinco){
-           System.out.println("A matricula com a maior nota foi a :" + vetores[2][0]);
+           System.out.println("A matricula com a maior nota foi a :" + matrizes[2][0]);
        } else if (alunoQuatro > alunoUm && alunoQuatro > alunoDois && alunoQuatro > alunoTres && alunoQuatro > alunoCinco){
-           System.out.println("A matricula com a maior nota foi a :" + vetores[3][0]);
+           System.out.println("A matricula com a maior nota foi a :" + matrizes[3][0]);
        } else if (alunoCinco > alunoUm && alunoCinco > alunoDois && alunoCinco > alunoTres && alunoCinco > alunoQuatro){
-           System.out.println("A matricula com a maior nota foi a :" + vetores[4][0]);
+           System.out.println("A matricula com a maior nota foi a :" + matrizes[4][0]);
        }
 
-       double mediaNotaF = (vetores [0][3] + vetores [1][3] + vetores[2][3] + vetores[3][3] + vetores[4][3]);
+       double mediaNotaF = (matrizes [0][3] + matrizes [1][3] + matrizes[2][3] + matrizes[3][3] + matrizes[4][3]);
        mediaNotaF /= 5;
 
        System.out.println();

b9a0a48a486a3e3e1d94bd5eae277434a4f5aa1b
Mateus de Castro <ttukstro@gmail.com>
2022-06-21 18:28:48 -0300

    correcao realizada em aula, com base nos gabaritos dos colegas

--- modulo1/Lista2/src/exercicio4.java
+++ modulo1/Lista2/src/exercicio4.java
@@ -5,22 +5,20 @@ public class exercicio4 {
         Scanner scanner = new Scanner(System.in);
 
         int [] numeros = new int[3];
-
+        int posicaoMenor = 0;
+        int con = Integer.MAX_VALUE;
 
         for (int i = 0; i < numeros.length; i++){
             System.out.println("Digite o valor " + i + ":");
             numeros[i] = scanner.nextInt();
             scanner.nextLine();
+        if (numeros[i] < con) {
+            posicaoMenor = i;
+            con = numeros[1];
             }
-        if (numeros[0] < numeros [1] && numeros [0] < numeros[2]) {
-            System.out.println("Menor numero é:" + numeros[0]);
-        } else if (numeros[1] < numeros [0] && numeros [1] < numeros[2]){
-            System.out.println("Menor numero é: " + numeros[1]);
-        } else {
-            System.out.println("Menor numero é: " + numeros[2]);
         }
 
-
+        System.out.println("A posição do menor numero é: " + posicaoMenor);
         scanner.close();
     }
 }
--- modulo1/Lista2/src/exercicio8.java
+++ modulo1/Lista2/src/exercicio8.java
@@ -7,39 +7,39 @@ public class exercicio8 {
         Scanner scanner = new Scanner(System.in);
 
         int[][] matrizes = new int[5][4];
+        int maiorNotaF = 0;
+        int matriculaMaiorNotaFinal = 0;
+        double mediaNotaF = 0;
 
-        for(int i = 0; i < 5; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.println("Digite o valor da linha " + (i + 1) + " e coluna" + (j + 1));
-                matrizes[i][j] = scanner.nextInt();
+
+        for(int i = 0; i < matrizes.length; i++) {
+            for (int j = 0; j < matrizes.length; j++) {
+                switch(j) {
+                    case 0 -> {
+                        System.out.println("Informe a matricula: " + (i + 1));
+                        matrizes[i][j] = scanner.nextInt();
+                    }
+                    case 1 -> {
+                        System.out.println("Informe a média das provas: ");
+                        matrizes[i][j] = scanner.nextInt();
+                    }
+                    case 2 -> {
+                        System.out.println("Informe a média dos trabalhaos: ");
+                        matrizes[i][j] = scanner.nextInt();
+                        mediaNotaF += (matrizes[i][1] * 0.6) + (matrizes[i][2] * 0.4);
+                        matrizes[i][j + 1] = (int)((matrizes[i][1] + 0.6) + (matrizes[i][2] * 0.4));
+                        if(matrizes[i][j + 1] > maiorNotaF){
+                            matriculaMaiorNotaFinal = matrizes[i][0];
+                            maiorNotaF = matrizes[i][j +1];
+                        }
+                    }
+                }
             }
         }
 
-       double alunoUm = (matrizes[0][1] * 0.6) + (matrizes[0][2] * 0.4);
-       double alunoDois = (matrizes[1][1] * 0.6) + (matrizes[1][2] * 0.4);
-       double alunoTres = (matrizes[2][1] * 0.6) + (matrizes[2][2] * 0.4);
-       double alunoQuatro = (matrizes[3][1] * 0.6) + (matrizes[3][2] * 0.4);
-       double alunoCinco = (matrizes[4][1] * 0.6) + (matrizes[4][2] * 0.4);
-
-       if(alunoUm > alunoDois && alunoUm > alunoTres && alunoUm > alunoQuatro && alunoUm > alunoCinco){
-           System.out.println("A matricula com a maior nota foi a :" + matrizes[0][0]);
-       } else if (alunoDois > alunoUm && alunoDois > alunoTres && alunoDois > alunoQuatro && alunoDois > alunoCinco) {
-           System.out.println("A matricula com a maior nota foi a :" + matrizes[1][0]);
-       } else if (alunoTres > alunoUm && alunoTres > alunoDois && alunoTres > alunoQuatro && alunoTres > alunoCinco){
-           System.out.println("A matricula com a maior nota foi a :" + matrizes[2][0]);
-       } else if (alunoQuatro > alunoUm && alunoQuatro > alunoDois && alunoQuatro > alunoTres && alunoQuatro > alunoCinco){
-           System.out.println("A matricula com a maior nota foi a :" + matrizes[3][0]);
-       } else if (alunoCinco > alunoUm && alunoCinco > alunoDois && alunoCinco > alunoTres && alunoCinco > alunoQuatro){
-           System.out.println("A matricula com a maior nota foi a :" + matrizes[4][0]);
-       }
-
-       double mediaNotaF = (matrizes [0][3] + matrizes [1][3] + matrizes[2][3] + matrizes[3][3] + matrizes[4][3]);
-       mediaNotaF /= 5;
-
-       System.out.println();
-       System.out.println("A media de notas finais foi: " + mediaNotaF);
-
-
+        mediaNotaF /= matrizes.length;
+        System.out.println("matricula com maior nota final: " + matriculaMaiorNotaFinal);
+        System.out.println("media notas finais: " + mediaNotaF);
     scanner.close();
     }
 }

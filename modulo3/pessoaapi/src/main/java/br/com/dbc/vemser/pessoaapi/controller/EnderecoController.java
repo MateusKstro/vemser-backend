3dcce8180d28c6056ac9d939c16407f363683dca
Mateus de Castro <ttukstro@gmail.com>
2022-07-12 19:17:36 -0300

    Homework7

--- modulo3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/EnderecoController.java
+++ modulo3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/EnderecoController.java
@@ -5,6 +5,10 @@ import br.com.dbc.vemser.pessoaapi.dto.EnderecoCreateDTO;
 import br.com.dbc.vemser.pessoaapi.dto.EnderecoDTO;
 import br.com.dbc.vemser.pessoaapi.entity.Endereco;
 import br.com.dbc.vemser.pessoaapi.service.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,33 +24,93 @@ public class EnderecoController {
     @Autowired
     private EnderecoService enderecoService;
 
+    @Operation(summary = "Listar endereco", description = "Lista todos os enderecos do banco")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de endercos"),
+                    @ApiResponse(responseCode = "400", description = "Não há enderecos a serem exibidas"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "listando endereco")
     @GetMapping
     public ResponseEntity<List<EnderecoDTO>> listarEndereco(){
         return ResponseEntity.ok(enderecoService.listarEnderecos());
     }
 
+    @Operation(summary = "Listar endereco por id", description = "Lista um endereco por id do banco de dados!")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de endereco por id"),
+                    @ApiResponse(responseCode = "400", description = "endereco nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Listando um endereco")
     @GetMapping("/{idEndereco}")
     public EnderecoDTO buscarEnderecoPorId(@PathVariable("idEndereco") Integer id) throws Exception{
         return enderecoService.buscarPorIdEndereco(id);
     }
 
+    @Operation(summary = "Listar endereco por idPessoa", description = "Lista um endereco por idPessoa do banco de dados!")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna endereco por idPessoa"),
+                    @ApiResponse(responseCode = "400", description = "Pessoa nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Listando um endereco por idPessoa")
     @GetMapping("/{idPessoa}/pessoa")
     public ResponseEntity<List<EnderecoDTO>> buscarEnderecoPorPessoa(@PathVariable("idPessoa") Integer id) throws Exception {
         return ResponseEntity.ok(enderecoService.buscarEnderecoPorIdPessoa(id));
     }
 
+    @Operation(summary = "Criar novo endereco", description = "Cria um novo endereco e insere no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Endereco criada"),
+                    @ApiResponse(responseCode = "400", description = "Requisicao inválida"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "criando endereco")
     @PostMapping("/{idPessoa}")
     public ResponseEntity<EnderecoDTO> create(@PathVariable("idPessoa") Integer idPessoa,
                                           @Valid @RequestBody EnderecoCreateDTO endereco) throws Exception {
         return ResponseEntity.ok(enderecoService.criarEndereco(idPessoa, endereco));
     }
 
+    @Operation(summary = "Editar endereco", description = "Edita um endereco e mantém no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Endereco editado"),
+                    @ApiResponse(responseCode = "400", description = "Endereco nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Editando um endereco")
     @PutMapping("/{idEndereco}")
     public ResponseEntity<EnderecoDTO> updateEndereco(@PathVariable("idEndereco") Integer idEndereco,
                           @Valid @RequestBody EnderecoCreateDTO endereco) throws Exception{
         return ResponseEntity.ok(enderecoService.atualizarEndereco(idEndereco, endereco));
     }
 
+    @Operation(summary = "Deletar endereco", description = "Deleta um endereco do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Endereco deletado"),
+                    @ApiResponse(responseCode = "400", description = "Endereco nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Deletando um endereco")
     @DeleteMapping("/{idEndereco}")
     public void delete(@PathVariable("idEndereco") Integer id) throws Exception {
         enderecoService.deletar(id);
--- modulo3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/PessoaController.java
+++ modulo3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/PessoaController.java
@@ -2,8 +2,11 @@ package br.com.dbc.vemser.pessoaapi.controller;
 
 import br.com.dbc.vemser.pessoaapi.dto.PessoaCreateDTO;
 import br.com.dbc.vemser.pessoaapi.dto.PessoaDTO;
-import br.com.dbc.vemser.pessoaapi.entity.Pessoa;
 import br.com.dbc.vemser.pessoaapi.service.PessoaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,32 +22,78 @@ public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
-    @GetMapping("/hello") // localhost:8080/pessoa/hello
-    public String hello() {
-        return "Hello world!";
-    }
 
+    @Operation(summary = "Criar nova pessoa", description = "Cria uma nova pessoa e insere no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Pessoa criada"),
+                    @ApiResponse(responseCode = "400", description = "Requisicao inválida"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping // localhost:8080/pessoa
+    @Schema(description = "criando pessoa")
     public ResponseEntity<PessoaDTO> create(@Valid @RequestBody PessoaCreateDTO pessoa) {
         return ResponseEntity.ok(pessoaService.create(pessoa));
     }
 
+    @Operation(summary = "Listar pessoas", description = "Lista todas as pessoas do banco")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de pessoas"),
+                    @ApiResponse(responseCode = "400", description = "Não há pessoas a serem exibidas"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping // localhost:8080/pessoa
+    @Schema(description = "listando pessoas")
     public ResponseEntity<List<PessoaDTO>> list() {
         return ResponseEntity.ok(pessoaService.list());
     }
 
+    @Operation(summary = "Listar pessoa por nome", description = "Lista uma pessoa por nome vinda do banco de dados!")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de pessoas"),
+                    @ApiResponse(responseCode = "400", description = "Pessoa nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Listando uma pessoa")
     @GetMapping("/byname") // localhost:8080/pessoa/byname?nome=Rafa
     public ResponseEntity<List<PessoaDTO>> listByName(@RequestParam("nome") String nome) {
         return ResponseEntity.ok(pessoaService.listByName(nome));
     }
 
+    @Operation(summary = "Editar pessoa", description = "Edita uma nova pessoa e mantém ela no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Pessoa editada"),
+                    @ApiResponse(responseCode = "400", description = "Pessoa nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Editando uma pessoa")
     @PutMapping("/{idPessoa}") // localhost:8080/pessoa/7
     public ResponseEntity<PessoaDTO> update(@PathVariable("idPessoa") Integer id,
                                          @Valid @RequestBody PessoaCreateDTO pessoaAtualizar) throws Exception {
         return ResponseEntity.ok(pessoaService.update(id, pessoaAtualizar));
     }
 
+    @Operation(summary = "Deletar pessoa", description = "Deleta uma nova pessoa do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Pessoa deletada"),
+                    @ApiResponse(responseCode = "400", description = "Pessoa nao existe"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @Schema(description = "Deletando uma pessoa")
     @DeleteMapping("/{idPessoa}") // localhost:8080/pessoa/10
     public void delete(@PathVariable("idPessoa") Integer id) throws Exception {
         pessoaService.delete(id);

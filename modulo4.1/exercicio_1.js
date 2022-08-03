6dfff59ab490d7c17bca4f3b63c0daf4bc34670b
Mateus de Castro <ttukstro@gmail.com>
2022-08-03 20:59:08 -0300

    homework1 e exercicio1 de mongoDB

---
+++ modulo4.1/exercicio_1.js
@@ -0,0 +1,31 @@
+[{
+  "_id": {
+    "$oid": "62eb0480eb3531cb483e5e0e"
+  },
+  "nome": "mateus de castro",
+  "data_nascimento": {
+    "$date": {
+      "$numberLong": "1003028400000"
+    }
+  }
+},{
+  "_id": {
+    "$oid": "62eb04efeb3531cb483e5e0f"
+  },
+  "nome": "pedro henrique",
+  "data_nascimento": {
+    "$date": {
+      "$numberLong": "849405600000"
+    }
+  }
+},{
+  "_id": {
+    "$oid": "62eb0512eb3531cb483e5e10"
+  },
+  "nome": "leandra oliveira",
+  "data_nascimento": {
+    "$date": {
+      "$numberLong": "1005098400000"
+    }
+  }
+}]
---
+++ modulo4.1/homework_1.js
@@ -0,0 +1,25 @@
+[{
+  "_id": {
+    "$oid": "62eb00b8eb3531cb483e5e0c"
+  },
+  "id_animal": 1,
+  "id_cliente": 1,
+  "nome": "Smith",
+  "tipo": "Cachorro",
+  "raca": "Pitbull",
+  "pelagem": "Curto",
+  "porte": "Medio",
+  "idade": 5
+},{
+  "_id": {
+    "$oid": "62eb00daeb3531cb483e5e0d"
+  },
+  "id_animal": 2,
+  "id_cliente": 2,
+  "nome": "Renatin pilantra",
+  "tipo": "Cachorro",
+  "raca": "Doberman",
+  "pelagem": "Curto",
+  "porte": "Grande",
+  "idade": 2
+}]
